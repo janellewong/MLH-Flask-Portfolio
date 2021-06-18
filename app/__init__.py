7170fd16350e2f483307d2ff590b1abc520b3936
Janelle Wong <janellewong112@gmail.com>
2021-06-18 13:48:52 -0700

    debugg

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,9 @@
 import os
 from flask import Flask, render_template, send_from_directory
 from dotenv import load_dotenv
-import locale
+# import locale
 
-locale.setlocale(locale.LC_ALL,'en_US.UTF-8')
+# locale.setlocale(locale.LC_ALL,'en_US.UTF-8')
 
 
 load_dotenv()
