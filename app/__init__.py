882910ebef3c33111ecb8c39b864c355e0083203
Janelle Wong <janellewong112@gmail.com>
2021-07-04 20:13:59 -0700

    database message

--- app/__init__.py
+++ app/__init__.py
@@ -39,7 +39,7 @@ class UserModel(db.Model):
 
 @app.route('/')
 def index():
-    return render_template('main.html', title="Janelle Wong | Portfolio", url=os.getenv("URL"))
+    return render_template('main.html', title="Janelle Wong | Portfolio", url=os.getenv("URL"), name="JANELLE")
 
 @app.route('/health')
 def health():
@@ -50,29 +50,30 @@ def register():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        db = get_db()
+
         error = None
 
         if not username:
             error = 'Username is required.'
         elif not password:
             error = 'Password is required.'
-        elif db.execute(
-            'SELECT id FROM user WHERE username = ?', (username,)
-        ).fetchone() is not None:
+        elif UserModel.query.filter_by(username=username).first() is not None:
             error = f"User {username} is already registered."
 
+        message = error
         if error is None:
-            db.execute(
-                'INSERT INTO user (username, password) VALUES (?, ?)',
-                (username, generate_password_hash(password))
-            )
-            db.commit()
-            return f"User {username} created successfully"
+            new_user = UserModel(username, generate_password_hash(password))
+            db.session.add(new_user)
+            db.session.commit()
+            message = f"User {username} created successfully."
+            return render_template('register.html', title="Janelle Wong  | ", url=os.getenv("URL"), 
+                message=message), 200
         else:
-            return error, 418
+            return render_template('register.html', title="Janelle Wong | ", url=os.getenv("URL"), 
+                message=message), 418
 
-    return render_template('register.html', title="Janelle Wong | register", url=os.getenv("URL"))
+    return render_template('register_template.html', title="Janelle Wong | ", url=os.getenv("URL"))
+
 
 
 @app.route('/login', methods=('GET', 'POST'))
@@ -80,20 +81,30 @@ def login():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        db = get_db()
+        user = UserModel.query.filter_by(username=username).first()
         error = None
-        user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
-        ).fetchone()
+
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        
+        if error is not None:
+            return render_template('login.html', title="Janelle Wong | ", url=os.getenv("URL"), 
+                message=error), 418
 
         if user is None:
-            error = 'Incorrect username.'
-        elif not check_password_hash(user['password'], password):
+            error = 'Username does not exist.'
+        elif not check_password_hash(user.password, password):
             error = 'Incorrect password.'
 
         if error is None:
-            return "Login Successful", 200 
+            return render_template('login.html', title="Janelle Wong | ", url=os.getenv("URL"), 
+                message="Login successful."), 200 
         else:
-            return error, 418
+            return render_template('login.html', title="Janelle Wong | ", url=os.getenv("URL"), 
+                message=error), 418
+    
     
     return render_template('login.html', title="Janelle Wong | login", url=os.getenv("URL"))
